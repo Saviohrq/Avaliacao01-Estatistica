f3f1d1231a655e0e4b9293d115e728bff1ce739d
Savio Henrique <saviobarradas@gmail.com>
2024-11-18 22:10:25 -0300

    media, moda e mediana funcionando

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,21 @@
+def dados(classes, frequencia):
+    numClass = len(classes)
+    fj = frequencia
+    somaFreq = sum(frequencia)
+    xj = []
+    freqAcumulada = []
+    freqAcumulada.append(fj[0])
+
+    for i in range(numClass):
+        a, b = classes[i]
+        xj.append((a + b) / 2)
+
+    for i in range(1, numClass):
+        freqAcumulada.append(fj[i] + fj[i-1])
+
+    return [numClass, fj, somaFreq, xj, freqAcumulada]
+    
+
 def media(x):
     return round(sum(x) / len(x), 2)
 
@@ -12,5 +30,13 @@ def mediaPond(pontoMedio, frequencia):
 def moda(pontoMedio, frequencia):
     return pontoMedio[frequencia.index(max(frequencia))]
 
-def mediana(media, moda):
-    return (2 * media + moda) / 3 # --> mediana obtida por meio da sua relação empírica
+def mediana(intervalos, frequencia, frequenciaAcumulada):
+    freqMediana = max(frequencia)
+    indexMediana = frequencia.index(freqMediana)
+    a, b = intervalos[indexMediana]
+    limiteInferior = a
+    amplitudeClasse = b - a
+    elementoMediano = sum(frequencia) / 2
+    freqAnterior = frequenciaAcumulada[(frequencia.index(freqMediana)) - 1]
+
+    return limiteInferior + amplitudeClasse * ((elementoMediano - freqAnterior) / freqMediana)
